1abb5585ff42327641800baef7d33bde9dd5b79a
himan7991 <himan7991@gmail.com>
2024-01-10 23:24:17 +0200

    add path aliases
    to make importing easier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/your_repo_here/' // if you intend to deploy this to github pages, change this to your project repo name
+	base: '/your_repo_here/', // if you intend to deploy this to github pages, change this to your project repo name
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	plugins: [react()]
 })
