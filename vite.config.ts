ac6c08ff9ff2af0e27ca75e3d19ba2f829836062
himan7991 <himan7991@gmail.com>
2024-01-22 21:51:29 +0200

    fix: path mapping now works with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src')
 		}
 	},
-	plugins: [react()]
+	plugins: [react(), tsconfigPaths()]
 })
